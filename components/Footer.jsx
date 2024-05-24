91e76d4bf168852aadb4e22afe1cbfcb505fa796
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-05-24 13:25:44 +0300

    add featured properties section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className='py-4 mt-auto bg-gray-200'>
+    <footer className='py-4 mt-24 bg-gray-200'>
       <div className='container flex flex-col items-center justify-between px-4 mx-auto md:flex-row'>
         <div className='mb-4 md:mb-0'>
           <Image
