a4e66079de8aa2c573f53ce0aee61b48c1f946eb
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-05-24 13:12:16 +0300

    add footer

---
+++ components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from '@/assets/images/logo.png';
+import Image from 'next/image';
+
+const Footer = () => {
+  return (
+    <footer className='py-4 mt-auto bg-gray-200'>
+      <div className='container flex flex-col items-center justify-between px-4 mx-auto md:flex-row'>
+        <div className='mb-4 md:mb-0'>
+          <Image
+            src={logo}
+            alt='Logo'
+            width={80}
+            height={80}
+            className='w-auto h-8'
+          />
+        </div>
+        <div className='flex flex-wrap justify-center mb-4 md:justify-start md:mb-0'>
+          <ul className='flex space-x-4'>
+            <li>
+              <a href='/properties.html'>Properties</a>
+            </li>
+            <li>
+              <a href='/terms.html'>Terms of Service</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className='mt-2 text-sm text-gray-500 md:mt-0'>
+            &copy; 2024 PropertyPulse. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
